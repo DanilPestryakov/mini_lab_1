093bcee3ee21e5045a26e23070e5e3c35671e77f
ilsaffff <justsairex@gmail.com>
2023-09-26 16:57:07 +0300

    refactoring: separation objects

---
+++ src/entries.py
@@ -0,0 +1,23 @@
+# class for entries storage (класс для хранения текстовых полей)
+from tkinter import Entry
+
+
+class Entries:
+    def __init__(self):
+        self.entries_list = []
+        self.parent_window = None
+
+    def set_parent_window(self, parent_window):
+        self.parent_window = parent_window
+
+    # adding of new entry (добавление нового текстового поля)
+    def add_entry(self):
+        new_entry = Entry(self.parent_window)
+        new_entry.icursor(0)
+        new_entry.focus()
+        new_entry.pack()
+        plot_button = self.parent_window.get_button_by_name('plot')
+        if plot_button:
+            plot_button.pack_forget()
+        self.parent_window.add_button('plot', 'Plot', 'plot', hot_key='<Return>')
+        self.entries_list.append(new_entry)
